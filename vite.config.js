0d2bc03e80582f213602ae248982d3f0f9f0664d
Whysabin <161411545+Whysabin@users.noreply.github.com>
2024-12-10 10:10:11 +0545

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import ghPages from 'gh-pages'; // Import the gh-pages package
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), ghPages()],
+  plugins: [react()],
   base: '/<Ochi-clone>/', // Replace <repository-name> with your repo name
 });
